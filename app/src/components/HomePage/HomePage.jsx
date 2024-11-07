22e45a466cb613280313527e9bf69cf14030b4c8
melo4yxa77796 <g_julija_777@mail.ru>
2024-11-07 20:58:06 +0100

    created Footer

--- app/src/components/HomePage/HomePage.jsx
+++ app/src/components/HomePage/HomePage.jsx
@@ -3,47 +3,40 @@ import hyfLogo from "../../assets/hyf.svg";
 import "./HomePage.css";
 import MealsList from "../../../frontend/components/MealsList";
 import { Button, Typography } from "@mui/material";
-import { Link } from "react-router-dom"; 
-
+import { Link } from "react-router-dom";
+import Footer from "../../../frontend/components/Footer";
 import Navbar from "../../../frontend/components/Navbar";
-
-
-
-
 
 // Feel free to replace the content of this component with your own
 function HomePage() {
   return (
-    
-    <> 
-    <Navbar />  
+    <>
+      <Navbar />
       <div className="home-page">
-     
-      <header className="header">
-        
-        <Typography variant="h1" gutterBottom>
-         Welcome to Meal-Sharing
-         
-        </Typography>
-        <Typography variant="h5" gutterBottom>
-        Discover delicious dishes from around the world!
-        </Typography>
-      </header>
+        <header className="header">
+          <Typography variant="h1" gutterBottom>
+            Welcome to Meal-Sharing
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            Discover delicious dishes from around the world!
+          </Typography>
+        </header>
 
-      {}
-      <section className="meals-section">
-        <MealsList  limit={6} /> { }
-      </section>
+        {}
+        <section className="meals-section">
+          <MealsList limit={6} /> {}
+        </section>
 
-      {}
-      <footer className="footer">
-        <Link to="/meals">
-          <Button variant="contained" color="primary">
-            View All Meals
-          </Button>
-        </Link>
-      </footer>
-    </div>
+        {}
+        <section className="">
+          <Link to="/meals">
+            <Button variant="contained" color="primary">
+              View All Meals
+            </Button>
+          </Link>
+        </section>
+        <Footer />
+      </div>
     </>
   );
 }
