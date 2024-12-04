28390d3bf18dcc44dbec8a4d43fe49ebfcf97628
melo4yxa77796 <g_julija_777@mail.ru>
2024-12-04 21:15:27 +0100

    change setting

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -1,4 +1,4 @@
-/*import knex from "knex";
+import knex from "knex";
 
 const connection = knex({
   client: process.env.DB_CLIENT,
@@ -13,21 +13,7 @@ const connection = knex({
   },
 });
 
-export default connection;*/
-import knex from 'knex';
-
-const connection = knex({
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
-    ssl: { rejectUnauthorized: false }  
-  },
-});
-
 export default connection;
+
 
 
