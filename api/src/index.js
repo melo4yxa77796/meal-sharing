e024e90b1b92bfd50b3905606763b00d86add93a
melo4yxa77796 <g_julija_777@mail.ru>
2024-12-04 12:14:59 +0100

    change cors

--- api/src/index.js
+++ api/src/index.js
@@ -13,11 +13,10 @@ import { fileURLToPath } from "url";
 import fs from "fs/promises";
 
 const app = express();
-app.use(cors({
-  origin: "https://meal-sharing-front.onrender.com",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-  credentials: true 
-}));
+app.use(cors(
+  "*"
+));
+
 app.use(bodyParser.json());
 
 const __filename = fileURLToPath(import.meta.url);
